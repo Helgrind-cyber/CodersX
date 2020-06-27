74b04492c0eb5be33efac1cd10c502890ee478be
helgrind-cyber <helgrindxxx@gmail.com>
2020-06-27 10:49:45 +0700

    Create views/users, res data from index.js to views

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -9,7 +9,13 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 app.get('/user', (req, res) => {
-    res.send('Nguyenb Hong Quan');
+    res.render('users/index', {
+        users: [
+            { name: 'Quan', age: 23, gender: 'male' },
+            {name: 'Quan', age: 18, gender: 'female'},
+            {name: 'Quan', age: 6, gender: 'unimpossible'}
+        ]
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
